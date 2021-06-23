000bcca8a7749bf95d0b196284fe63e744ef830b
t32rahman <t10rahman@ryerson.ca>
2021-06-23 08:59:32 -0400

    small changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,17 +1,17 @@
-#Courtesy of Felix Yu @Felix Yu on Youtube
-#!/bin/bash
+# #Courtesy of Felix Yu @Felix Yu on Youtube
+# #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+# #give permission for everything in the express-app directory
+# sudo chmod -R 777 /home/ec2-user/express-app
 
-#navigate into our working directory where we have all our github files
-cd
-cd /express-app
+# #navigate into our working directory where we have all our github files
+# cd
+# cd /express-app
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# #add npm and node to path
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
